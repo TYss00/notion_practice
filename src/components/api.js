534fda6f6c67cd137025d6741588008788aeecff
TYss00 <rlaxodus203@naver.com>
2025-04-03 15:41:06 +0900

    비동기로 바꿔줌

--- src/components/api.js
+++ src/components/api.js
@@ -1,24 +1,26 @@
 const API_URL = 'https://kdt-api.fe.dev-cos.com/documents';
 const HEADERS = {
     'Content-Type': 'application/json',
-    'x-username': 'TYss0000', // 여기에 api이름 넣기
+    'x-username': 'TYs', // 여기에 api이름 넣기
 };
 
 // 문서 목록 가져오기
-function fetchDocuments() {
-    return fetch(API_URL, {
+async function fetchDocuments() {
+    const res = await fetch(API_URL, {
         method: 'GET',
         headers: HEADERS,
-    }).then((res) => res.json());
+    });
+    return await res.json();
 }
 
 // 문서 생성하기
-function createDocument(title, parent = null) {
-    return fetch(API_URL, {
+async function createDocument(title, parent = null) {
+    const res = await fetch(API_URL, {
         method: 'POST',
         headers: HEADERS,
         body: JSON.stringify({ title, parent }),
-    }).then((res) => res.json());
+    });
+    return await res.json();
 }
 
 // 전역 객체 등록
